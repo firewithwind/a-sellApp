393c5ab60fdff73d89539a768a10fc8b84c578b7
firewithwind <17862702878@163.com>
2017-08-02 19:36:16 +0800

    seller of app

--- src/App.vue
+++ src/App.vue
@@ -12,26 +12,34 @@
         <router-link to="/seller">商家</router-link>
       </div>
     </div>
-    <router-view :seller="seller"></router-view>
+    <keep-alive>
+      <router-view :seller="seller"></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import header from 'components/header/header.vue'
+import {urlParse} from 'common/js/util.js'
 
 const REE_OK = 0
 
 export default {
   data() {
     return {
-      seller: {}
+      seller: {
+        id: (() => {
+          let queryParam = urlParse()
+          return queryParam.id
+        })()
+      }
     }
   },
   created() {
-    this.$http.get('/api/seller').then((res) => {
+    this.$http.get('/api/seller?id=' + this.seller.id).then((res) => {
       res = res.body
       if (res.errno === REE_OK) {
-        this.seller = res.data
+        this.seller = Object.assign({}, this.seller, res.data)
       }
     })
   },
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -131,23 +131,23 @@
 					font-size: 12px
 				.support
 					.icon
-						display: inline-block
-						vertical-align: top
-						width: 12px
-						height: 12px
-						margin-right: 4px
-						background-size: 12px 12px
-						background-repeat: no-repeat
-						&.decrease
-							bg-image('decrease_1')
-						&.discount
-							bg-image('discount_1')
-						&.guarantee
-							bg-image('guarantee_1')
-						&.invoice
-							bg-image('invoice_1')
-						&.special
-							bg-image('special_1')
+							display: inline-block
+							vertical-align: top
+							width: 12px
+							height: 12px
+							margin-right: 4px
+							background-size: 12px 12px
+							background-repeat: no-repeat
+							&.decrease
+								bg-image('decrease_1')
+							&.discount
+								bg-image('discount_1')
+							&.guarantee
+								bg-image('guarantee_1')
+							&.invoice
+								bg-image('invoice_1')
+							&.special
+								bg-image('special_1')
 					.text
 						line-height: 12px
 						font-size: 10px
